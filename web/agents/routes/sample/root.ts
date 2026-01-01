bfc17196f36f5d3e9abb02f91fe62e3278c18a72
Giali Coletta <51245135+Gialicus@users.noreply.github.com>
2026-01-01 23:23:48 +0100

    Create root.ts

---
+++ web/agents/routes/sample/root.ts
@@ -0,0 +1,32 @@
+import { google } from "@ai-sdk/google";
+import { tool, ToolLoopAgent } from "ai";
+import { type FastifyInstance, type FastifyPluginOptions } from 'fastify'
+import z from "zod";
+
+export default async function (fastify: FastifyInstance, opts: FastifyPluginOptions) {
+  const agent =  new ToolLoopAgent({
+    model: google("gemini-3-flash-preview"),
+    instructions: "You are a helpful assistant that can answer questions and help with tasks.",
+    tools: {
+        get_weather: tool({
+            description: "Get the weather for a given location",
+            inputSchema: z.object({ location: z.string() }),
+            execute: async ({ location }) => {
+                return { weather: "sunny" }
+            }
+        })
+    }
+  });
+
+  fastify.get('/', async (request, reply) => {
+    const { textStream } = await agent.stream({
+        prompt: 'What is the weather in Rome?',
+    });
+    let text = '';
+    for await (const chunk of textStream) {
+        fastify.log.info(chunk);
+        text += chunk;
+    }
+    return { text }
+  })
+}
